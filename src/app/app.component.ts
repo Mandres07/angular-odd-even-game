13f7652caac765728111be2285ab70af5dc3dd26
Mario Morales <mario.morales@devsu.com>
2021-09-30 08:18:00 -0500

    game finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+   oddNumbers: number[] = [];
+   evenNumbers: number[] = [];
+
+   onIntervalFired(value: number) {
+      if (value % 2 === 0) {
+         this.evenNumbers.push(value);
+      }
+      else {
+         this.oddNumbers.push(value);
+      }
+   }
 }
